6165031e2f1599b55c272c3a0de71a7ca7ef8249
ointhe <ointhe@gmail.com>
2019-03-16 10:55:24 +0900

    test components

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-
-import FirstPage from './components/pages/First';
-
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.navigator = null;
-  }
   render() {
-    
-      const routeConfig = {
-        First: {
-          screen: FirstPage,
-          path: 'first',
-        },
-        // Second: {
-        //   screen: SecondPage,
-        //   path: 'second',
-        // },
-      };
-    const Navigator = createStackNavigator(routeConfig, {
-      initialRouteName: startPage,
-      header: null,
-      headerMode: 'none',
-      navigationOptions: {
-        header: null,
-      },
-    });
-    const AppContainer = createAppContainer(Navigator);
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
